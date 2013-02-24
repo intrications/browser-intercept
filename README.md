b48928d73f55540f3afbc588abe0ac1b7e61786d
Michael Basil <michael@intrications.com>
2013-02-24 13:59:54 +0000

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ browser-intercept
 =================
 
 Browser Intercept Android App
+
+https://play.google.com/store/apps/details?id=com.intrications.android.sharebrowse
+
+## License
+Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0.html)
---
+++ src/com/intrications/android/sharebrowser/MainActivity.java
@@ -0,0 +1,20 @@
+package com.intrications.android.sharebrowser;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		Intent sendIntent = new Intent();
+		sendIntent.setAction(Intent.ACTION_SEND);
+		sendIntent.putExtra(Intent.EXTRA_TEXT, getIntent().getDataString());
+		sendIntent.setType("text/plain");
+		startActivity(Intent.createChooser(sendIntent, "Share"));
+		finish();
+	}
+}
